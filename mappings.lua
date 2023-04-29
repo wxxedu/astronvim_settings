50397b1f6bb8e4c61c6294042d3f30aa89d79452
Xiuxuan Wang <36021591+wxxedu@users.noreply.github.com>
2023-04-29 20:08:52 +0800

    add flutter and dart

--- mappings.lua
+++ mappings.lua
@@ -23,7 +23,7 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
-    ["<leader>ll"] = { ":VimtexCompile" }
+    ["<leader>ll"] = { ":VimtexCompile<cr>" }
   },
   t = {
     -- setting a mapping to false will disable it
---
+++ plugins/flutter.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    'dart-lang/dart-vim-plugin'
+  },
+  {
+    'thosakwe/vim-flutter'
+  }
+}
