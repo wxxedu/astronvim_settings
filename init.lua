6ae4aa7ad1741554b5d151119debd931a7df1031
Xiuxuan Wang <36021591+wxxedu@users.noreply.github.com>
2023-05-06 15:32:37 +0800

    update snippets

--- init.lua
+++ init.lua
@@ -64,6 +64,13 @@ return {
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
   plugins = {
+    {
+      "L3MON4D3/LuaSnip",
+      config = function(plugin, opts)
+        require "plugins.configs.luasnip" (plugin, opts)                                       -- include the default astronvim config that calls the setup call
+        require("luasnip.loaders.from_vscode").lazy_load { paths = { "./lua/user/snippets" } } -- load snippets paths
+      end,
+    },
   },
   polish = function()
     -- Set up custom filetypes
