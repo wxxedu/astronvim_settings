34d8e37ad39c4ab8ac25bc7b0cbc8e2601166eb7
Xiuxuan Wang <36021591+wxxedu@users.noreply.github.com>
2023-05-31 07:12:05 +0800

    feat(flutter): add support for flutter

--- plugins/flutter.lua
+++ plugins/flutter.lua
@@ -1,44 +1,14 @@
 return {
-  -- {
-  --   'dart-lang/dart-vim-plugin'
-  -- },
-  -- {
-  --   'thosakwe/vim-flutter'
-  -- },
-  -- {
-  --   'akinsho/flutter-tools.nvim',
-  --   lazy = false,
-  --   dependencies = {
-  --     'nvim-lua/plenary.nvim',
-  --     'stevearc/dressing.nvim', -- optional for vim.ui.select
-  --   },
-  --   config = function()
-  --     require('flutter-tools').setup {
-  --       flutter_path = '/Users/xiuxuan/Developer/flutter/bin/flutter',
-  --       widget_guides = {
-  --         enabled = true,
-  --       },
-  --       closing_tags = {
-  --         highlight = 'ErrorMsg',
-  --       },
-  --       dev_log = {
-  --         enabled = true,
-  --         notify_errors = false,
-  --         open_cmd = 'tabedit',
-  --       },
-  --       outline = {
-  --         open_cmd = '30vnew',
-  --       },
-  --       dev_tools = {
-  --         autostart = true,         -- autostart devtools server if not detected
-  --         auto_open_browser = true, -- Automatically opens devtools in the browser
-  --       },
-  --       lsp = {
-  --         on_attach = function(client, bufnr)
-  --           require('lsp').on_attach(client, bufnr)
-  --         end,
-  --       },
-  --     }
-  --   end,
-  -- }
+  {
+    'dart-lang/dart-vim-plugin'
+  },
+  {
+    'thosakwe/vim-flutter'
+  },
+  {
+    'natebosch/vim-lsc'
+  },
+  {
+    'natebosch/vim-lsc-dart'
+  }
 }
